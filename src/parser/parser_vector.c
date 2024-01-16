d32ac2637bd63d7fcb3cf568760515729aec258f
Thomas Hofting <thofting@c4a8c3.42berlin.de>
2024-01-16 14:16:51 +0100

    Corrected error in validity check

--- src/parser/parser_vector.c
+++ src/parser/parser_vector.c
@@ -47,20 +47,20 @@ int is_vector(char *str)
 	int		pos_second;
 
 	pos_first = get_pos_nth_char(',', str, 1);
-	pos_second = get_pos_nth_char(',', str, 1);
+	pos_second = get_pos_nth_char(',', str, 2);
 	if (!(pos_first > -1 && pos_first <  MAX_LEN_DOUBLE))
-		return (-1);
+		return (0);
 	ft_strlcpy(temp, str, pos_first + 1);
 	if (!is_valid_double(temp))
-		return (-1);
+		return (0);
 	ft_strlcpy(temp, str + pos_first + 1, \
-		pos_second - pos_first + 1);
+		pos_second - pos_first);
 	if (!is_valid_double(temp))
-		return (-1);
+		return (0);
 	ft_strlcpy(temp, str + pos_second + 1, \
-		ft_strlen(str) - pos_second + 1);
+		ft_strlen(str) - pos_second - 1);
 	if (!is_valid_double(temp))
-		return (-1);
+		return (0);
 	return (1);
 }
 
