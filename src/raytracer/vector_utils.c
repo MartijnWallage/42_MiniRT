32032bb1fa5881fd49e13745d57c7947287abe38
Martijn Wallage <mwallage@student.42berlin.de>
2024-01-18 21:32:23 +0100

    Update

--- src/raytracer/vector_utils.c
+++ src/raytracer/vector_utils.c
@@ -6,15 +6,16 @@
 /*   By: mwallage <mwallage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 16:37:26 by mwallage          #+#    #+#             */
-/*   Updated: 2024/01/18 17:07:38 by mwallage         ###   ########.fr       */
+/*   Updated: 2024/01/18 21:31:52 by mwallage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT"
 
+void	cross(t_vector a, t_vector b, t_vector result);
+void	dot(t_vector a, t_vector b, t_vector result);
+void	add(t_vector a, t_vector b, t_vector result);
+void	subtract(t_vector a, t_vector b, t_vector result);
+void	scalar_multiply(t_vector a, double scalar, t_vector result);
+double	norm(t_vector vec);
 void	normalize(t_vector input, t_vector output);
-void	cross(t_vector a, t_vector b, t_vector output);
-void	dot(t_vector a, t_vector b, t_vector output);
-void	add(t_vector a, t_vector b, t_vector output);
-void	multiply(t_vector a, double scalar, t_vector output);
-double	vec_len(t_vector vec);
