24477d2bbef9a1125de4b221a8a3ac6919ea364f
Martijn Wallage <mwallage@student.42berlin.de>
2024-01-20 17:36:50 +0100

    Still trying

--- src/raytracer/raytracer.c
+++ src/raytracer/raytracer.c
@@ -6,7 +6,7 @@
 /*   By: mwallage <mwallage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 16:02:05 by mwallage          #+#    #+#             */
-/*   Updated: 2024/01/20 17:30:00 by mwallage         ###   ########.fr       */
+/*   Updated: 2024/01/20 17:36:12 by mwallage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,7 @@ void	compute_ray(t_minirt *minirt, int x, int y, t_ray *ray)
 	t_vector	product_up_y;
 	t_vector	sum;
 	t_object	*iter;
+	double		dx, dy;
 
 	camera = minirt->scene->camera;
 /* 	ndc[0] = x / minirt->image->width;			// convert to normal device coordinates
@@ -77,18 +78,18 @@ void	compute_ray(t_minirt *minirt, int x, int y, t_ray *ray)
 	scaley = tan(camera->fov / 2);
 	vpc[0] = (2 * ndc[0] - 1) * scalex;			// convert to view plane coordinates
 	vpc[1] = (1 - 2 * ndc[1]) * scaley; */
-	x = (double)x / minirt->image->width;
-	y = (double)y / minirt->image->height;
-	x = 2.0 * x - 1.0;
-	y = 2.0 * y - 1.0;
-	x *= camera->width;
-	y *= camera->height;
-	multiply(camera->right, x, product_right_x);
-	multiply(camera->up, y, product_up_y);
+	dx = (double)x / minirt->image->width;
+	dy = (double)y / minirt->image->height;
+	dx = 2.0 * dx - 1.0;
+	dy = 2.0 * dy - 1.0;
+	dx *= camera->width;
+	dy *= camera->height;
+	multiply(camera->right, dx, product_right_x);
+	multiply(camera->up, dy, product_up_y);
 	add(product_right_x, product_up_y, sum);
 	add(sum, camera->normvect, ray->normvect);
 	normalize(ray->normvect, ray->normvect);
-//	printf("Ray %d, %d: %f, %f, %f\n", x, y, ray->normvect[0], ray->normvect[1], ray->normvect[2]);
+	printf("Ray %d, %d: %f, %f, %f\n", x, y, ray->normvect[0], ray->normvect[1], ray->normvect[2]);
 	iter = minirt->scene->objects;
 	ray->intersection = 0;
 	ray->object = NULL;
